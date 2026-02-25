113a4dbac8e63ce334f44507a6940848b4a322cc
XPenguinGodX <amadeo.2001@gmail.com>
2026-02-24 23:27:33 -0700

    Perk system done
    UI disappears and game starts over

--- scripts/game/perks/double_jump.gd
+++ scripts/game/perks/double_jump.gd
@@ -1,18 +1,23 @@
 extends Node2D
 
 @onready var character = get_node("/root/Game/Character")
-@onready var perklist = get_node("Character/Camera2D/UI/Panel/BoxContainer")
+@onready var perkList = get_node("/root/Game/Character/Camera2D/UI")
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	print(perkList)
 	$Panel/AnimatedSprite2D.play("default")
 
 
 func _on_button_pressed() -> void:
 	character.enable_double_jump()
 	character.back_to_start()
-	%BoxContainer.visibility = false
+	perkList.visible = false
+	get_tree().paused = false
+	
+	
+	
 	
 	

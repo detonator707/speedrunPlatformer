43ef89063cc18636af665a80d19bf1339c81a2f1
XPenguinGodX <amadeo.2001@gmail.com>
2026-03-04 08:33:40 -0700

    timer
    Timer logic works, just need to get it to show

---
+++ scripts/game/timer.gd
@@ -0,0 +1,22 @@
+extends Timer
+
+@onready var label: Label = %Label
+@onready var UI = get_node("/root/Game/Character/Camera2D/UI")
+
+var timeInSeconds: int = 0
+
+func _ready() -> void:
+	UI.visible = true
+	timeout.connect(_on_timer_timeout) 
+	print("Timer _ready() called – connecting signal and starting...")
+	start()
+	print("Timer started – wait time:", wait_time, " one_shot:", one_shot)                             
+
+func _on_timer_timeout() -> void:
+	timeInSeconds += 1
+	print("TIMEOUT FIRED ", timeInSeconds)
+	var minutes = timeInSeconds / 60
+	var seconds = timeInSeconds % 60
+	
+	label.text = "%02d:%02d" % [minutes, seconds]
+	print("Labe text set to", label.text)

ce9e48d853cafe54c55f0af1173dd162998fc7e8
XPenguinGodX <amadeo.2001@gmail.com>
2026-03-08 20:20:59 -0600

    panel done
    panel is visible time to code

--- scripts/game/game.gd
+++ scripts/game/game.gd
@@ -2,9 +2,14 @@ extends Node
 
 #perks
 @onready var doubleJump = load("res://Scenes/perks/doubleJump.tscn")
+@onready var characterlist = get_node("/root/Game/Character/Camera2D/UI/Panel2/BoxContainer")
 @onready var dash = load("res://Scenes/perks/Dash.tscn")
-
+@onready var finn = load("res://Scenes/dolphin.tscn")
+@onready var peng = load("res://Scenes/penguin.tscn")
+@onready var dog = load("res://Scenes/zeke.tscn")
+@onready var mush = load("res://Scenes/mush.tscn")
 @onready var perkList: Array[PackedScene] = [doubleJump, dash]
+@onready var characters: Array[PackedScene] = [finn,dog,peng,mush]
 
 #obstacles
 @onready var fog = load("res://Scenes/obstacles/fog.tscn")
@@ -36,6 +41,16 @@ func getRound():
 
 func _ready() -> void:
 	perkList.shuffle()
+	var finn2 = characters[0].instantiate()
+	var dog2 = characters[1].instantiate()
+	var peng2 = characters[2].instantiate()
+	var mush2 = characters[3].instantiate()
+
+	characterlist.get_child(0).add_child(finn2)
+	characterlist.get_child(1).add_child(peng2)
+	characterlist.get_child(2).add_child(dog2)
+	characterlist.get_child(3).add_child(mush2)
+	
 	
 	var insta = perkList[0].instantiate()
 	%BoxContainer.get_child(0).add_child(insta)

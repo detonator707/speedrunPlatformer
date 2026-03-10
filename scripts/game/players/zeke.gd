a9b4c0be22480b71ddfff1f087c89fe445c763dc
detonator707 <149191513+detonator707@users.noreply.github.com>
2026-03-10 11:44:54 -0600

    added penguin

--- scripts/game/players/zeke.gd
+++ scripts/game/players/zeke.gd
@@ -1,4 +1,5 @@
 extends Node2D
+@onready var character = get_node("/root/Game/Character")
 @onready var panel = get_node("/root/Game/Character/Camera2D/UI/Panel2")
 @onready var box = get_node("/root/Game/Character/Camera2D/UI/Panel2/BoxContainer")
 
@@ -13,5 +14,6 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
+	character.sprite = ""
 	panel.visible = false
 	box.visible = false 
